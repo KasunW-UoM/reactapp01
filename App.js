3e9a15f73d56e36f787ba0fb04fa488810c560de
Kasun madhulal weerasekara <73052192+KasunW-UoM@users.noreply.github.com>
2021-03-21 02:15:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>Kasun  </h1>
+<h2>React APP</h2>
+    
+    </div>
+  );
+}
+
+export default App;
